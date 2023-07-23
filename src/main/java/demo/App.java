8d40c925d54f55ef19dfce025a6332dddb652ae2
huygiale <huygiale2202@gmail.com>
2023-07-23 15:09:41 +0700

    demo-gradle-jenkins

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,13 @@
+package demo;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
